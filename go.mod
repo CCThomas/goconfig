2ae404ebf46eac0fe99ddf743389bbd619178e37
Christopher Thomas <ccthom94@gmail.com>
2021-11-25 17:46:56 -0600

    Database Gorm

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/CCThomas/goconfig
 go 1.16
 
 require (
-  github.com/joho/godotenv v1.4.0 // indirect
-  github.com/sirupsen/logrus v1.8.1 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	gorm.io/gorm v1.22.3 // indirect
 )
---
+++ pkg/databases/gorm.databases.go
@@ -0,0 +1,31 @@
+package databases
+
+import (
+  os
+
+  log "github.com/sirupsen/logrus"
+  "gorm.io/gorm"
+)
+
+var GormDB *gorm.DB
+
+func ConfigureGorm() {
+  log.Info("Try to connect to database.")
+  host := os.Getenv("GOCONFIG_DB_HOST")
+  user := os.Getenv("GOCONFIG_DB_USER")
+  password := os.Getenv("GOCONFIG_DB_PASSWORD")
+  dbname := os.Getenv("GOCONFIG_DB_NAME")
+  port := os.Getenv("GOCONFIG_DB_PORT")
+  sslmode := os.Getenv("GOCONFIG_DB_SSLMODE")
+
+  dsn := fmt.Sprintf(
+    "host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=America/Chicago",
+    host, user, password, dbname, port, sslmode)
+  db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+  if err != nil {
+    log.Fatal("failed to connect database")
+  }
+
+  log.Infof("Connected to %s.\n", host)
+  GormDB = db
+}
